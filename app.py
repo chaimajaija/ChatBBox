3f4d5d728d7eb20012242926654e3cac322a52db
chaimajaija <85251351+chaimajaija@users.noreply.github.com>
2024-05-03 11:55:28 +0100

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import google.generativeai as genai
 import google.ai.generativelanguage as glm
 
 ## Function To get response from LLAma 2 model
-GOOGLE_API_KEY = os.environ['AIzaSyCc_Luvg7lE0MSZFbq9uxlEu1eM_teTFmw']
+GOOGLE_API_KEY = os.environ[GOOGLE_API_KEY]
 
 genai.configure(api_key=GOOGLE_API_KEY)
 model = genai.GenerativeModel(model_name='gemini-pro')
